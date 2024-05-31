2452de21cecbb182a0009fc903d9149d60053ca2
Mohamed  Ashraf <67920757+Mohamedart13@users.noreply.github.com>
2024-06-01 01:11:39 +0300

    Alibaba internship from placementdost company
    Alibaba internship from placementdost company

---
+++ EXP_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# import the data from csv file.
+df = pd.read_csv("D:/projects/internships/placementdost_company/SQL/SQL/Alibaba_Data.csv",header=0)
+df
